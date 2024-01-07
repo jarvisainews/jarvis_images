0cfae56c8239f2be4b98f67cf76aff17ecf10778
Carter Susi <carter4299@gmail.com>
2024-01-07 17:01:54 -0600

    lol

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ for service in services:
   first_char = service[0]
   if first_char.isdigit():
     os.mkdir(f"./#/{service}/") if not os.path.exists(f"./#/{service}/") else None
+    os.mknod(f"./#/{service}/placeholder.bin") 
     with open (f"./#/.current.toml", "a") as f:
       f.write(f"[{service}]\n")
       f.write("square = \n")
@@ -15,7 +16,7 @@ for service in services:
     f.close()
   else:
     os.mkdir(f"./{first_char.upper()}/{service}/") if not os.path.exists(f"./{first_char.upper()}/{service}/") else None
-    os.remove(f"./{first_char.upper()}/current.toml") if os.path.exists(f"./{first_char.upper()}/current.toml") else None
+    os.mknod(f"./{first_char.upper()}/{service}/placeholder.bin")
     with open (f"./{first_char.upper()}/.current.toml", "a") as f:
       f.write(f"[{service}]\n")
       f.write("square = \n")
