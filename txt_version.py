093227cda2e85e9dd994a136f67084c2a423a51e
Carter Susi <carter4299@gmail.com>
2024-01-09 16:40:05 -0600

    auto

--- txt_version.py
+++ txt_version.py
@@ -13,3 +13,13 @@ with open("./current.txt", "w") as f:
         f.write(f"[{key}]\n{service_link}\n{image_search}\nSquare: {square}\nLong: {long}\n\n")
 
 
+out_toml = {}
+with open('test.toml', 'w') as f:
+    for key in current_toml:
+        f.write(f"[{key}]\n")
+        f.write(f"\t[links]\n")
+        f.write(f"\t\t{current_toml[key]['image_search']}\n")
+        f.write(f"\t\t{current_toml[key]['service_link']}\n")
+        f.write(f"\t[images]\n")
+        f.write(f"\t\tsquare = {current_toml[key]['square']}\n")
+        f.write(f"\t\tlong = {current_toml[key]['long']}\n\n")
