92fa0d7c3f0b964c38fd301c708c246e29adcc22
Carter Susi <carter4299@gmail.com>
2024-01-09 16:09:37 -0600

    ...

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Current Images for JarvisAI
+
+![Jarvis's](./.support/jarvis_office.jpeg)
+
+## Index:
+-  Currently on -> 10xllm
+
+## Finished:
+-  Total 0.0981%  |  5 / 5098
+-  Square 0.1569%  |  4 / 2549
+-  Long 0.0392%  |  1 / 2549
+
+## Needed:
+-  Total 99.9019%  |  5093 / 5098
+-  Square 99.8431%  |  2545 / 2549
+-  Long 99.9608%  |  2548 / 2549
---
+++ update.py
@@ -0,0 +1,56 @@
+import tomli
+import os
+
+current_toml = tomli.load(open("./current.toml", "rb"))
+
+readme = '''
+# Current Images for JarvisAI
+
+![Jarvis's](./.support/jarvis_office.jpeg)
+
+## Index:
+-  Currently on -> {}
+
+## Finished:
+-  Total {}%  |  {} / 5098
+-  Square {}%  |  {} / 2549
+-  Long {}%  |  {} / 2549
+
+## Needed:
+-  Total {}%  |  {} / 5098
+-  Square {}%  |  {} / 2549
+-  Long {}%  |  {} / 2549
+'''
+
+needed, finished = [], []
+
+long = [key for key in current_toml if current_toml[key]["long"] == " "]
+square = [key for key in current_toml if current_toml[key]["square"] == " "]
+needed.extend(long)
+needed.extend(square)
+
+long_ = [key for key in current_toml if current_toml[key]["long"] != " "]
+square_ = [key for key in current_toml if current_toml[key]["square"] != " "]
+finished.extend(long_)
+finished.extend(square_)
+
+with open("./README.md", "w") as f:
+    f.write(readme.format(
+        needed[0],
+        round((len(finished)/(len(current_toml)*2))*100, 4), len(finished),
+        round((len(square_)/len(current_toml)*100),4), len(square_),
+        round((len(long_)/len(current_toml)*100),4), len(long_),
+        round((len(needed)/(len(current_toml)*2))*100, 4), len(needed),
+        round((len(square)/len(current_toml)*100),4), len(square),
+        round((len(long)/len(current_toml)*100),4), len(long)
+    ))
+f.close()
+
+needed, finished = set(needed), set(finished)
+with open("./.support/needed.txt", "w") as f:
+    f.write("\n".join(needed))
+f.close()
+
+with open("./.support/finished.txt", "w") as f:
+    f.write("\n".join(finished))
+f.close()
